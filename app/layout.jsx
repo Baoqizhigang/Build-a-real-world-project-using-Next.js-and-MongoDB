38524db1613974976876985af79df183969f87cf
Joe Liu <13301215+Baoqizhigang@users.noreply.github.com>
2025-05-20 19:58:08 -0700

    no error

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,21 @@
-import '@/assets/styles/globals.css'
+import Navbar from "@/components/Navbar";
+import "@/assets/styles/globals.css";
 
 export const metadata = {
-    title: "Property Pulse",
-    keywords: 'Rental, property, real estate',
-    description: 'Find the perfect rental property',
+  title: "Property Pulse",
+  keywords: "Rental, property, real estate",
+  description: "Find the perfect rental property",
 };
 
 const MainLayout = ({ children }) => {
-    return (<html> 
-        <body>
-            <main>{children}</main>
-        </body>
-    </html>);
-}
- 
+  return (
+    <html>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
 export default MainLayout;
